3f3712e8f434b615ab44e6d8c31e56505ad64063
notzerotwo_ <63092138+NotZer0Two@users.noreply.github.com>
2025-07-09 21:15:32 +0200

    Updated

--- PlaceholderAPI/PlaceholderAPIPlugin.cs
+++ PlaceholderAPI/PlaceholderAPIPlugin.cs
@@ -34,7 +34,7 @@
         public override string Name => "PlaceholderAPI";
 
         /// <inheritdoc/>
-        public override Version Version => new (1, 0, 3);
+        public override Version Version => new (1, 0, 4);
 
         /// <inheritdoc/>
         public override string Author => "NotZer0Two";
